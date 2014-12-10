787b8d5183e3138e1d302e832ac17d97013ec4e8
Luiz Carlos da F. Jr. <luiz.bruntec@gmail.com>
2014-12-10 20:01:19 -0200

    configuração de acesso a base dados no arquivo database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -79,10 +79,10 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'test_dblicita',
 		'prefix' => '',
-		//'encoding' => 'utf8',
+		'encoding' => 'utf8',
 	);
 }
